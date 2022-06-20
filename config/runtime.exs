bb761c96ea302f5d73740c80f902e4bb514ca591
mhoffnn <matheushofmarck@gmail.com>
2022-06-20 16:13:14 -0300

    configurando banco

--- config/runtime.exs
+++ config/runtime.exs
@@ -27,7 +27,6 @@ if config_env() == :prod do
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
-    sslmode: "require"
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
